8980fdb1af6c6a7f8ffd3038434fcbb0678acd9e
ejsadiarin <ejsadiarin@gmail.com>
2023-09-27 02:52:16 +0800

    navbar and hero section initial

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,23 @@ export default function Home() {
   return (
     <body className='w-full h-full'>
       {/* Navbar */}
-      <nav className='flex justify-around items-center w-full h-12'>
-        <Image src='/tarkinge.svg' alt='Tarkinge Logo' width='50' height='50' />
+      <nav className='flex justify-around items-center w-full h-12 border-2 border-b-gray-500'>
+        <Image src='/tarkinge.svg' alt='Tarkinge Logo' width='40' height='40' />
         <p className='font-bold text-lg'>Tarkinge</p>
-        <HamburgerMenu />
+        <div className='flex gap-5'>
+          <h3 className='font-bold'>Docs</h3>
+          <HamburgerMenu />
+        </div>
       </nav>
 
       {/* Hero */}
-      <main>
-        <article>test</article>
-        <Button>Next</Button>
+      <main className='m-20'>
+        <article className='mb-4'>
+          <p>Powered by AI</p>
+          <h1 className='font-bold'>Supercharge your design workflow</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores natus perferendis nemo, inventore molestias id placeat veritatis at veniam et harum beatae? Nesciunt magnam optio autem architecto rerum, ut mollitia.</p>
+        </article>
+        <Button className='w-auto h-auto font-bold px-5 py-3'>Start for free</Button>
       </main>
 
     </body>
